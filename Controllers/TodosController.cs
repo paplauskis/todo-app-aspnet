d4e1c1867bfb3b9f89d145fecfc4cee732f29585
Kostas Paplauskas <paplauskaskostas@gmail.com>
2024-10-20 19:56:16 +0300

    added POST endpoints to create todos

--- Controllers/TodosController.cs
+++ Controllers/TodosController.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
+using todo_app.Domain.DTOs;
 using todo_app.Domain.Interfaces.Service;
+using todo_app.Domain.Models;
 using todo_app.Utilities;
 
 namespace todo_app.Controllers;
@@ -25,7 +27,42 @@ public class TodosController : ControllerBase
         var timedTodos = await _timedTodoService.GetAllAsync();
         var todos1 = TodoMapper.Convert(simpleTodos);
         var todos2 = TodoMapper.Convert(timedTodos);
-
+        
         return Ok(todos1.Concat(todos2));
+    }
+    
+    [HttpPost("simple-todo")]
+    public async Task<IActionResult> Create([FromBody] CreateSimpleTodoDTO? simpleTodoDto)
+    {
+        if (simpleTodoDto == null)
+            return BadRequest("Invalid data.");
+        
+        var newTodo = new SimpleTodo(
+            simpleTodoDto.Name,
+            simpleTodoDto.Priority,
+            simpleTodoDto.Description,
+            simpleTodoDto.Image
+        );
+
+        await _simpleTodoService.CreateAsync(newTodo);
+
+        return Ok(newTodo);
+    }
+    
+    [HttpPost("timed-todo")]
+    public async Task<IActionResult> Create([FromBody] CreateTimedTodoDTO? timedTodoDto)
+    {
+        if (timedTodoDto == null)
+            return BadRequest("Invalid data.");
+        
+        var newTodo = new TimedTodo(
+            timedTodoDto.Name,
+            timedTodoDto.DueDate,
+            timedTodoDto.Description
+        );
+
+        await _timedTodoService.CreateAsync(newTodo);
+
+        return Ok(newTodo);
     }
 }
