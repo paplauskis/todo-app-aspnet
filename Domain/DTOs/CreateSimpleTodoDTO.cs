b347084d1452b2ecd49553148272510efeeaf8b3
Kostas Paplauskas <paplauskaskostas@gmail.com>
2024-10-20 19:52:26 +0300

    made Name property non nullable

--- Domain/DTOs/CreateSimpleTodoDTO.cs
+++ Domain/DTOs/CreateSimpleTodoDTO.cs
@@ -5,7 +5,7 @@ namespace todo_app.Domain.DTOs;
 // dto used when need to read request to create todo
 public class CreateSimpleTodoDTO
 {
-    public string? Name { get; set; }
+    public string Name { get; set; }
     public string? Description { get; set; }
     public PriorityLevel Priority { get; set; }
     public string? Image { get; set; }
