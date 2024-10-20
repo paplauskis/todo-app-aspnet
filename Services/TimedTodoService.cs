230892fd59f68e34eb44c52f00c3867cce8c48b9
Kostas Paplauskas <paplauskaskostas@gmail.com>
2024-10-20 15:24:43 +0300

    implemented service with basic crud methods

---
+++ Services/TimedTodoService.cs
@@ -0,0 +1,59 @@
+using todo_app.Domain.Interfaces.Repository;
+using todo_app.Domain.Interfaces.Service;
+using todo_app.Domain.Models;
+
+namespace todo_app.Services;
+
+public class TimedTodoService : ITimedTodoService
+{
+    private readonly ITimedTodoRepository _repository;
+
+    public TimedTodoService(ITimedTodoRepository repo)
+    {
+        _repository = repo;
+    }
+
+    public async Task<IEnumerable<TimedTodo>> GetAllAsync()
+    {
+        return await _repository.GetAll();
+    }
+
+    public async Task<TimedTodo> GetByIdAsync(string id)
+    {
+        return await _repository.GetById(id);
+    }
+
+    public async Task<TimedTodo> CreateAsync(TimedTodo todo)
+    {
+        return await _repository.Create(todo);
+    }
+
+    public async Task<TimedTodo> UpdateAsync(TimedTodo todo)
+    {
+        var todoToBeUpdated = await GetByIdAsync(todo.Id.ToString());
+
+        if (todoToBeUpdated == null)
+        {
+            throw new Exception("todo not found.");
+        }
+
+        todoToBeUpdated.Name = todo.Name;
+        todoToBeUpdated.Description = todo.Description;
+
+        await _repository.Update(todoToBeUpdated);
+        
+        return todoToBeUpdated;
+    }
+
+    public async Task<TimedTodo> DeleteAsync(string id)
+    {
+        var todoToBeDeleted = await GetByIdAsync(id);
+
+        if (todoToBeDeleted == null)
+        {
+            throw new Exception("todo not found");
+        }
+
+        return await _repository.Delete(todoToBeDeleted);
+    }
+}
