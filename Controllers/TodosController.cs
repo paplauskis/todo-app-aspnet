92920784f1febaf75ad3e40d75e21ca87b70902b
Kostas Paplauskas <paplauskaskostas@gmail.com>
2024-10-20 15:07:31 +0300

    added services and root GET endpoint

--- Controllers/TodosController.cs
+++ Controllers/TodosController.cs
@@ -1,10 +1,31 @@
 using Microsoft.AspNetCore.Mvc;
+using todo_app.Domain.Interfaces.Service;
+using todo_app.Utilities;
 
 namespace todo_app.Controllers;
 
 [ApiController]
-[Route("[controller]")]
+[Route("/api/[controller]")]
 public class TodosController : ControllerBase
 {
-    
+    //later: implement one service, repository for both timed and simple by using generic type
+    private readonly ISimpleTodoService _simpleTodoService;
+    private readonly ITimedTodoService _timedTodoService;
+
+    public TodosController(ISimpleTodoService simpleTodoService, ITimedTodoService timedTodoService)
+    {
+        _simpleTodoService = simpleTodoService;
+        _timedTodoService = timedTodoService;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAllTodos()
+    {
+        var simpleTodos = await _simpleTodoService.GetAllAsync();
+        var timedTodos = await _timedTodoService.GetAllAsync();
+        var todos1 = TodoConverter.Convert(simpleTodos);
+        var todos2 = TodoConverter.Convert(timedTodos);
+
+        return Ok(todos1.Concat(todos2));
+    }
 }
