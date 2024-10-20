58a97068db092838ad88e635e49ea19198352118
Kostas Paplauskas <paplauskaskostas@gmail.com>
2024-10-20 15:14:24 +0300

    added createdAt property to database

---
+++ Data/Migrations/20241020105121_Added-CreatedAt.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace todo_app.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedCreatedAt : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "created_at",
+                table: "timed_todo",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "created_at",
+                table: "simple_todo",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "created_at",
+                table: "timed_todo");
+
+            migrationBuilder.DropColumn(
+                name: "created_at",
+                table: "simple_todo");
+        }
+    }
+}
