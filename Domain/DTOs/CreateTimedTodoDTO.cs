cabcee12eaa00999db400c5f543ff59161d8ce46
Kostas Paplauskas <paplauskaskostas@gmail.com>
2024-10-20 19:53:02 +0300

    made Name non-nullable, added due_date property name

--- Domain/DTOs/CreateTimedTodoDTO.cs
+++ Domain/DTOs/CreateTimedTodoDTO.cs
@@ -1,10 +1,13 @@
+using System.Text.Json.Serialization;
+
 namespace todo_app.Domain.DTOs;
 
 // dto used when need to read request to create todo
 public class CreateTimedTodoDTO
 {
-    public string? Name { get; set; }
+    public string Name { get; set; }
     public string? Description { get; set; }
+    [JsonPropertyName("due_date")]
     public DateTime DueDate { get; set; }
 
     public CreateTimedTodoDTO(string name, DateTime dueDate, string description = null)
