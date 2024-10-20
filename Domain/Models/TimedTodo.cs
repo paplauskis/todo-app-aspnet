2e71a3f8811739c995f60b05b1ef466f1fc3ac04
Kostas Paplauskas <paplauskaskostas@gmail.com>
2024-10-20 15:23:28 +0300

    created TimedTodo model

---
+++ Domain/Models/TimedTodo.cs
@@ -0,0 +1,32 @@
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace todo_app.Domain.Models;
+
+public class TimedTodo : Todo
+{
+    
+    private DateTime _dueDate;
+    
+    [JsonPropertyName("due_date")]
+    [Column("due_date")]
+    public DateTime DueDate
+    {
+        get => _dueDate;
+        set
+        {
+            if (value < CreatedAt)
+                throw new ArgumentException("Due date cannot be earlier than todo creation date.");
+            _dueDate = value;
+        }
+    }
+
+    public TimedTodo(string name, DateTime dueDate, string? description = null)
+    {
+        Id = Guid.NewGuid();
+        CreatedAt = DateTime.Now;
+        Name = name;
+        Description = description;
+        DueDate = dueDate;
+    }
+}
