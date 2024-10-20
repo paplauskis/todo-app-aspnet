09ca117094bc3ad10f1841a91e9e0dd65c57c5d6
Kostas Paplauskas <paplauskaskostas@gmail.com>
2024-10-20 15:05:13 +0300

    added SimpleTodos and TimedTodos DbSet

--- Data/Context/AppDbContext.cs
+++ Data/Context/AppDbContext.cs
@@ -1,11 +1,26 @@
 using Microsoft.EntityFrameworkCore;
-using todo_app.Models;
+using todo_app.Domain.Models;
 
-namespace todo_app.Data;
+namespace todo_app.Data.Context;
 
-public class TodoDbContext : DbContext
+public class AppDbContext : DbContext
 {
-    public TodoDbContext(DbContextOptions options) : base(options) { }
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+    
+    public DbSet<SimpleTodo> SimpleTodos { get; set; }
+    public DbSet<TimedTodo> TimedTodos { get; set; }
 
-    public DbSet<Todo> Todos { get; set; }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        
+        modelBuilder.Entity<SimpleTodo>()
+            .ToTable("simple_todo")
+            .HasKey(s => s.Id);
+        
+        modelBuilder.Entity<TimedTodo>()
+            .ToTable("timed_todo")
+            .HasKey(t => t.Id);
+    }
+    
 }
