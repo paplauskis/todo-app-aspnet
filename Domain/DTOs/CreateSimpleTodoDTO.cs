a7a67baf7dc16e39ce1e90df2c0d8c5fe1b64e23
Kostas Paplauskas <paplauskaskostas@gmail.com>
2024-10-20 18:22:58 +0300

    renamed DTO class

--- Domain/DTOs/CreateSimpleTodoDTO.cs
+++ Domain/DTOs/CreateSimpleTodoDTO.cs
@@ -3,23 +3,20 @@ using todo_app.Domain.Enumerations;
 namespace todo_app.Domain.DTOs;
 
 // dto used when need to read request to create todo
-public class SimpleTodoDTO
+public class CreateSimpleTodoDTO
 {
     public string? Name { get; set; }
     public string? Description { get; set; }
     public PriorityLevel Priority { get; set; }
     public string? Image { get; set; }
-    public bool IsCompleted { get; set; }
 
-    public SimpleTodoDTO(
+    public CreateSimpleTodoDTO(
         string name, 
-        bool isCompleted, 
         PriorityLevel priority, 
         string description = null, 
         string image = null)
     {
         Name = name;
-        IsCompleted = isCompleted;
         Priority = priority;
         Description = description;
         Image = image;
--- Domain/DTOs/CreateTimedTodoDTO.cs
+++ Domain/DTOs/CreateTimedTodoDTO.cs
@@ -1,17 +1,15 @@
 namespace todo_app.Domain.DTOs;
 
 // dto used when need to read request to create todo
-public class TimedTodoDTO
+public class CreateTimedTodoDTO
 {
     public string? Name { get; set; }
     public string? Description { get; set; }
-    public bool IsCompleted { get; set; }
     public DateTime DueDate { get; set; }
 
-    public TimedTodoDTO(string name, bool isCompleted, DateTime dueDate, string description = null)
+    public CreateTimedTodoDTO(string name, DateTime dueDate, string description = null)
     {
         Name = name;
-        IsCompleted = isCompleted;
         DueDate = dueDate;
         Description = description;
     }
