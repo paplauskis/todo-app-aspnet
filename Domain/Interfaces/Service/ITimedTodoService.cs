054693ca152794204535adb56f0b33f41bc26b50
Kostas Paplauskas <paplauskaskostas@gmail.com>
2024-12-09 15:04:19 +0200

    changed UpdateAsync parameters

--- Domain/Interfaces/Service/ITimedTodoService.cs
+++ Domain/Interfaces/Service/ITimedTodoService.cs
@@ -1,3 +1,4 @@
+using todo_app.Domain.DTOs;
 using todo_app.Domain.Models;
 
 namespace todo_app.Domain.Interfaces.Service;
@@ -7,6 +8,6 @@ public interface ITimedTodoService
     Task<IEnumerable<TimedTodo>> GetAllAsync();
     Task<TimedTodo> GetByIdAsync(string id);
     Task<TimedTodo> CreateAsync(TimedTodo todo);
-    Task<TimedTodo> UpdateAsync(TimedTodo todo);
+    Task<TimedTodo> UpdateAsync(UpdateTimedTodoDTO todo, string id);
     Task<TimedTodo> DeleteAsync(string id);
 }
