73d8c1c336bddf709b27c0e2f9ed5a85564590dc
Kostas Paplauskas <paplauskaskostas@gmail.com>
2024-12-09 15:11:49 +0200

    removed Id property

--- Domain/DTOs/UpdateSimpleTodoDTO.cs
+++ Domain/DTOs/UpdateSimpleTodoDTO.cs
@@ -4,7 +4,6 @@ namespace todo_app.Domain.DTOs;
 
 public class UpdateSimpleTodoDTO
 {
-    public Guid Id { get; set; }
     public string? Name { get; set; }
     public string? Description { get; set; }
     public PriorityLevel Priority { get; set; }
@@ -12,14 +11,12 @@ public class UpdateSimpleTodoDTO
     public bool IsCompleted { get; set; }
 
     public UpdateSimpleTodoDTO(
-        Guid id,
         string name, 
         bool isCompleted, 
         PriorityLevel priority,
-        string description = null, 
-        string image = null)
-    { 
-        Id = id;
+        string description,
+        string image)
+    {
         Name = name;
         IsCompleted = isCompleted;
         Priority = priority;
--- Domain/DTOs/UpdateTimedTodoDTO.cs
+++ Domain/DTOs/UpdateTimedTodoDTO.cs
@@ -2,20 +2,17 @@ namespace todo_app.Domain.DTOs;
 
 public class UpdateTimedTodoDTO
 {
-    public Guid Id { get; set; }
     public string? Name { get; set; }
     public string? Description { get; set; }
     public DateTime DueDate { get; set; }
     public bool IsCompleted { get; set; }
 
     public UpdateTimedTodoDTO(
-        Guid id,
         string name,
         DateTime dueDate,
         bool isCompleted,
-        string description = null)
+        string description)
     {
-        Id = id;
         Name = name;
         DueDate = dueDate;
         IsCompleted = isCompleted;
